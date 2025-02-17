1457bc73278807eaa30b5f3a81e2880213af9f90
Alexander12xx <168762078+Alexander12xx@users.noreply.github.com>
2025-02-17 15:25:40 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Smooth Scrolling for Navbar Links
+document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener('click', function(event) {
+        event.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
+    });
+});
